31a31ec99f40dc1d572773982c1ad7bf43b45b42
Aditya Shinde <adityashinde4580@gmail.com>
2023-05-30 09:34:49 +0530

    Create expt6_InetAddress.java

---
+++ expt6_InetAddress.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.net.InetAddress;
+
+public class expt6_InetAddress {
+
+	public static void main(String [] args) {
+		
+		try {
+			InetAddress ip = InetAddress.getByName("www.sinhgad.edu");
+			System.out.println("Host Name: " + ip.getHostName());
+			System.out.println("IP Address: " + ip.getHostAddress());
+			}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+	
