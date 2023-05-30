4a9267fc706b4a7f1c6a1e7f2dde6e576b26f1c4
Aditya Shinde <adityashinde4580@gmail.com>
2023-05-30 09:52:07 +0530

    expt8_Jdbc

---
+++ expt8/InsertingData.java
@@ -0,0 +1,36 @@
+package expt8;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertingData {
+ static final String DB_URL = "jdbc:mysql://localhost:3306/STUDENTS2";
+ static final String USER = "root";
+ static final String PASS = "root123";
+ public static void main(String[] args) throws ClassNotFoundException {
+ // Open a connection
+try
+ {
+ Class.forName("com.mysql.jdbc.Driver");
+ Connection conn=DriverManager.getConnection(
+ DB_URL, USER, PASS);
+
+ Statement stmt=conn.createStatement();
+ // Execute a query
+ System.out.println("Inserting records into the table...");
+ String sql = "INSERT INTO REG VALUES (100, 'Zara', 'Ali', 18)";
+ stmt.executeUpdate(sql);
+ sql = "INSERT INTO REG VALUES (101, 'Mahnaz', 'Fatma', 25)";
+ stmt.executeUpdate(sql);
+ sql = "INSERT INTO REG VALUES (102, 'Zaid', 'Khan', 30)";
+ stmt.executeUpdate(sql);
+ sql = "INSERT INTO REG VALUES(103, 'Sumit', 'Mittal', 28)";
+ stmt.executeUpdate(sql);
+ System.out.println("Inserted records into the table...");
+ } catch (SQLException e) {
+ e.printStackTrace();
+ }
+ }
+}
